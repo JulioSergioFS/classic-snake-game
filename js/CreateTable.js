5a85ba73cd7af13e9209f19b0533c19d9f3c7e43
Julio Sergio <juliosergio010302@gmail.com>
2020-12-14 22:38:14 -0300

    creating the table

--- js/CreateTable.js
+++ js/CreateTable.js
@@ -2,10 +2,28 @@ class CreateTable{
     
     constructor(){
         this._arrayOfTheTable = [];
-        this._amountOfLines = 0;
-        this._amountOfColumns = 0;
+        this._amountOfLines = 18;
+        this._amountOfColumns = 18;
     }
-    
+
+    create(element){
+        let table = "<table>";
+        
+        for (let row = 1; row < this._amountOfColumns; row++) {
+            
+            table += "<tr>";
+
+            for (let column = 1; column < this._amountOfColumns; column++) {
+                table += "<td>";
+                table += "</td>";
+            }
+
+            table += "</tr>";
+        }
+
+        table += "</table>";
+        element.innerHTML = table;
+    }
 }
 
 
