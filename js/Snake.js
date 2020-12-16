ff9ee470395710f3fbcdfba610dc0174ab31e197
Julio Sergio <juliosergio010302@gmail.com>
2020-12-15 22:40:18 -0300

    Making the snake move

--- js/Snake.js
+++ js/Snake.js
@@ -1,10 +1,45 @@
 class Snake{
+    constructor(){
+        this._position = 1;
+        this._td = '';
+    }
+
+    _changeTd(spawn){
+        if (!spawn) {
+            this._td.classList.remove("snake");
+        }
+        this._td = document.querySelector(`#td${this._position}`);
+        this._td.classList.add("snake");
+    }
 
     spawn(row, column){ // linha 2 coluna 1
-        let position = (row*20)+(column);
-        let td = document.querySelector(`#td${position}`);
-        td.classList.add("snake");
-        console.log(position);
-        console.log(td);
+        this._position = (row*20)+(column);
+        this._changeTd(true);
+        console.log(this._td);
+    }
+
+    walk(){
+        document.addEventListener('keyup', (event) => {
+
+            if (event.key == 'ArrowUp') {
+                this._position = this._position - 20;
+                this._changeTd(false);
+            }
+
+            if (event.key == 'ArrowLeft') {
+                this._position = this._position - 1;
+                this._changeTd(false);
+            }
+            
+            if (event.key == 'ArrowDown') {
+                this._position = this._position + 20;
+                this._changeTd(false);
+            }
+            
+            if (event.key == 'ArrowRight') {
+                this._position++;
+                this._changeTd(false);
+            }
+        });
     }
 }
