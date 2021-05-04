8e6bb3d3ac388f1c8c0949750b3508d51531de53
Julio Sergio <juliosergio010302@gmail.com>
2021-05-03 21:34:51 -0300

    Deleted useless argument

--- js/Controller.js
+++ js/Controller.js
@@ -6,7 +6,7 @@ export class Controller {
     constructor(table){
         this._table = new CreateTable().create(document.querySelector(table));
         this._snake = new Snake()
-        this._spawn = this._snake.spawn(9, 9,this._table);
+        this._spawn = this._snake.spawn(9, 9);
         this._walk = this._snake.walk();
     }
 }
